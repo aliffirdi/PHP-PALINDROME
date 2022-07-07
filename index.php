6a82cc5829679a5bafd8c5527dafd68bbe77ce71
alif firdi <45675831+aliffirdi@users.noreply.github.com>
2022-07-08 04:21:29 +0700

    adding palindrome variation coding styles 3 & 4

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ $isPalindrome = new palindrome();
 // return TRUE
 var_dump($isPalindrome->checkv1("ada"));
 var_dump($isPalindrome->checkv2("ada"));
+var_dump($isPalindrome->checkv3("ada"));
+var_dump($isPalindrome->checkv4("ada"));
 // return FALSE
 var_dump($isPalindrome->checkv1("kepo"));
 var_dump($isPalindrome->checkv2("kepo"));
+var_dump($isPalindrome->checkv3("kepo"));
+var_dump($isPalindrome->checkv4("kepo"));
--- palindrome.class.php
+++ palindrome.class.php
@@ -28,8 +28,53 @@ class palindrome
             $lastIndex  = strlen($value) - $i - 1;
 
             if ($value[$firstIndex] != $value[$lastIndex]) {
+
                 return false;
+                
             }
+
+        }
+
+        return true;
+
+    }
+
+    public function checkv3 ($value=null)
+    {
+
+        $arrNum = (int)((strlen($value))-1);
+
+        for ($i=$arrNum; $i >= 0; $i--) {
+
+            $firstIndex = $i;
+            $lastIndex  = strlen($value) - $i - 1;
+
+            if ($value[$firstIndex] != $value[$lastIndex]) {
+
+                return false;
+
+            }
+
+        }
+
+        return true;
+
+    }
+
+    public function checkv4 ($value=null)
+    {
+
+        $reverse = null;
+
+        for ($i=strlen($value)-1; $i >= 0; $i--) {
+
+            $reverse .= $value[$i];
+
+        }
+        
+        if ($value != $reverse) {
+
+            return false;
 
         }
 
